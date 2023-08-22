c4ccf181f5c4c3c33bbcd332041c2faa95ac4ab8
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-22 22:21:04 +0500

    7.1.7 Впишите пропущенный код для параметров в Console.WriteLine вместо ??? так, чтобы в консоли выводилось "Привет, Грег, я интеллектуальный помощник Олег":
    Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", ???);

---
+++ module7.1.7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/// <summary>
+/// Впишите пропущенный код для параметров в Console.WriteLine вместо ??? так, чтобы в консоли выводилось "Привет, Грег, я интеллектуальный помощник Олег":
+/// Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", ???);
+/// </summary>
+namespace module7._1._7
+{
+    class SmartHelper
+    {
+        private string name;
+
+        public SmartHelper(string name)
+        {
+            this.name = name;
+        }
+
+        public void Greetings(string name)
+        {
+            Console.WriteLine("Привет, {0}, я интеллектуальный помощник {1}", name, this.name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartHelper helper = new SmartHelper("Олег");
+            helper.Greetings("Грег");
+
+            Console.ReadKey();
+        }
+
+    }
+}
