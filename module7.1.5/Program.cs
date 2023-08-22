b6c1910f9e0d21927f4abeafcaaa248bfeb9963f
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-22 22:13:24 +0500

    7.1.5 Для следующего списка объектов создайте схему классов (объявите нужные классы и установите связи между ними):
    Apple (яблоко); Banana (банан); Pear (груша); Potato (картофель); Carrot (морковь).

---
+++ module7.1.5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+/// <summary>
+/// Для следующего списка объектов создайте схему классов (объявите нужные классы и установите связи между ними):
+///     Apple(яблоко)
+///     Banana(банан);
+///     Pear(груша);
+///     Potato(картофель);
+///     Carrot(морковь).
+/// </summary>
+namespace module7._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Fruit : Food
+    {
+
+    }
+    class Vegetable : Food
+    {
+
+    }
+    class Food
+    {
+
+    }
+    class Apple : Fruit
+    {
+
+    }
+    class Banana : Fruit
+    {
+
+    }
+    class Pear : Fruit
+    {
+
+    }
+    class Potato : Vegetable
+    {
+
+    }
+    class Carrot : Vegetable
+    {
+
+    }
+}
