3f3e4620fa6e0b7d7f1b1915db21fd89aed56b02
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-22 22:18:16 +0500

    7.1.6 Реализуйте конструктор, заполняющий поля класса Obj:
    private string name;  private string owner; private int length; private int count;

---
+++ module7.1.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary>
+/// 7.1.6 Реализуйте конструктор, заполняющий поля класса obj:
+///         private string name; 
+///         private string owner;
+///         private int length;
+///         private int count;
+/// </summary>
+namespace module7._1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName, int objLength, int count)
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+        }
+    }
+
+}
