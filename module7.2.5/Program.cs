814e68972c9b5fab86a5f9e2deda63b07d3a9a63
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-29 19:02:21 +0500

    7.2.5 Вернитесь к заданию 7.2.3 и дополните его код так, чтобы для вызова следующего следующего кода в консоль выводилось 2 сообщения (сначала "Метод класса BaseClass", а затем "Метод класса DerivedClass"):

---
+++ module7.2.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace module7._2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass obj = new DerivedClass();
+            obj.Display();
+        }
+    }
+    class BaseClass
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Метод класса BaseClass");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Метод класса DerivedClass");
+        }
+    }
+}
