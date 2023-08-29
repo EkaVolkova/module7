c3df4e5ffb4599934ef172da14d53b3b831bc7ff
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-29 18:52:46 +0500

    7.2.4
    Измените свойство Counter так, чтобы его можно было переопределить в классе DerivedClass. Переопределите данное свойство, ограничив занесения в него чисел меньше 0.

---
+++ module7.2.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/// <summary>
+/// Измените свойство Counter так, чтобы его можно было переопределить в классе DerivedClass. 
+/// Переопределите данное свойство, ограничив занесения в него чисел меньше 0.
+/// </summary>
+namespace module7._2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class BaseClass
+    {
+        public virtual int Counter
+        {
+            get;
+            set;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        private int counter;
+        public override int Counter
+        {
+            get => counter;
+            set
+            {
+                if (value >= 0)
+                    counter = value;
+            }
+        }
+    }
+}
