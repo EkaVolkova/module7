2dd5eeed58c74f20e534ac8e043947cb83431880
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-29 18:47:29 +0500

    7.2.3 Реализуйте в классе BaseClass виртуальный метод Display с типом void и без параметров, который будет выводить сообщение "Метод класса BaseClass" в консоль, а затем переопределите его в DerivedClass, чтобы он выводил сообщение "Метод класса DerivedClass".

---
+++ module7.2.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// Реализуйте в классе BaseClass виртуальный метод Display с типом void и без параметров, который будет выводить сообщение "Метод класса BaseClass" в консоль,
+/// а затем переопределите его в DerivedClass, чтобы он выводил сообщение "Метод класса DerivedClass".
+/// </summary>
+namespace module7._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class BaseClass
+        {
+            public virtual void Display()
+            {
+                Console.WriteLine("Метод класса BaseClass");
+            }
+        }
+
+        class DerivedClass : BaseClass
+        {
+            public override void Display()
+            {
+                Console.WriteLine("Метод класса DerivedClass");
+            }
+        }
+
+    }
+}
