4e97953ea4ee2c76c3a6b3645e852ae59c7e33cd
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-30 18:57:02 +0500

    7.3.3 Создайте классы для следующих объектов компьютера: процессор (Processor), материнская карта (MotherBoard), видеокарта (GraphicCard). Унаследуйте их от класса ComputerPart.
    Добавьте в класс ComputerPart абстрактный метод Work без параметров и с типом void.

---
+++ module7.3.3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/// <summary>
+/// Создайте классы для следующих объектов компьютера: процессор (Processor), материнская карта (MotherBoard), видеокарта (GraphicCard). 
+/// Унаследуйте их от класса ComputerPart.
+/// Добавьте в класс ComputerPart абстрактный метод Work без параметров и с типом void.
+/// </summary>
+namespace module7._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+    class Processor : ComputerPart
+    {
+        public override void Work()
+        {
+            
+        }
+    }
+    class MotherBoard : ComputerPart
+    {
+        public override void Work()
+        {
+            
+        }
+    }
+
+    class GraphicCard : ComputerPart
+    {
+        public override void Work()
+        {
+            
+        }
+    }
+}
