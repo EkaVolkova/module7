c2d89f110dafcda2bd132d61504648f68701f841
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-30 18:42:47 +0500

    7.2.12 Для класса Obj перегрузите операторы + и -, чтобы результатом работы оператора был новый экземпляр класса Obj, а операции производились над полем Value.

---
+++ module7.2.12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/// <summary>
+/// Для класса Obj перегрузите операторы + и -, чтобы результатом работы оператора был новый экземпляр класса Obj, а операции производились над полем Value.
+/// </summary>
+namespace module7._2._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Obj
+    {
+        public int Value;
+
+        public static Obj operator +(Obj a, Obj b)
+        {
+            return new Obj
+            {
+                Value = a.Value + b.Value
+            };
+        }
+        public static Obj operator -(Obj a, Obj b)
+        {
+            return new Obj
+            {
+                Value = a.Value - b.Value
+            };
+        }
+    }
+}
