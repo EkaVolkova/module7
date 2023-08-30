fd2046911968c6d8fa20c011d8e1defcb63ab557
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-30 18:50:59 +0500

    7.2.14 Для следующего класса напишите индексатор, для типа параметра используйте int:
    class IndexingClass
    {
      private int[] array;
    
      public IndexingClass(int[] array)
      {
        this.array = array;
      }
    }

---
+++ module7.2.14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Для следующего класса напишите индексатор, для типа параметра используйте int:
+/// </summary>
+namespace module7._2._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                    return array[index];
+                return 0;
+            }
+            set
+            {
+                if (index >= 0 && index < array.Length)
+                    array[index] = value;
+            }
+        }
+
+    }
+}
