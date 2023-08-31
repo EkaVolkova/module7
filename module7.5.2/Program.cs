3121a18a83af36a025b2318e33bd08bc92902234
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-31 19:50:21 +0500

    7.5.2 Создайте класс Obj, определите в нем поля Name, Description (тип строки) и статическое поле MaxValue типа int, равное 2000.

---
+++ module7.5.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Создайте класс Obj, определите в нем поля Name, Description (тип строки) и статическое поле MaxValue типа int, равное 2000.
+/// </summary>
+namespace module7._5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Obj
+    {
+        public string Name;
+        public string Descriphion;
+        public static int MaxValue = 2000;
+    }
+}
