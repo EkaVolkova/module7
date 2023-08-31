56d50432fd9a3ed4dd8ce97addbc990c7cc89bc2
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-31 22:10:52 +0500

    7.6.2 Создайте класс-обобщение Car для автомобиля. Универсальным параметром будет тип двигателя в автомобиле (электрический и бензиновый). Для типов двигателей также создайте классы — ElectricEngine и GasEngine. В классе Car создайте поле Engine в качестве типа которому укажите универсальный параметр.

---
+++ module7.6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary>
+/// Создайте класс-обобщение Car для автомобиля. Универсальным параметром будет тип двигателя в автомобиле (электрический и бензиновый). 
+/// Для типов двигателей также создайте классы — ElectricEngine и GasEngine.
+/// В классе Car создайте поле Engine в качестве типа которому укажите универсальный параметр.
+/// </summary>
+namespace module7._6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Car<T>
+    {
+        public T Engine = default;
+    }
+    class ElectricEngine
+    {
+
+    }
+    class GasEngine
+    {
+
+    }
+
+}
