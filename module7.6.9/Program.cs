3d66114b74c4edb67af4b6f757ef9594a4ebd031
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-04 19:32:49 +0500

    7.6.9 Установите ограничения на универсальные типы в классе Car. Такие, чтобы поле Engine могло принимать тип ElectricEngine и GasEngine, а параметр newPart метода ChangePart мог бы принимать только типы частей машины (Battery, Differential, Wheel).

---
+++ module7.6.9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+/// <summary>
+/// Установите ограничения на универсальные типы в классе Car (7.6.7). Такие, чтобы поле Engine могло принимать тип ElectricEngine и GasEngine,
+/// а параметр newPart метода ChangePart мог бы принимать только типы частей машины (Battery, Differential, Wheel).
+/// </summary>
+namespace module7._6._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Car<T1> where T1 : Engine
+    {
+        public T1 Engine = default;
+        public virtual void ChangePart<T2>(T2 newPart) where T2 : PartCar
+        {
+
+        }
+    }
+    class Engine
+    {
+
+    }
+    class PartCar
+    {
+
+    }
+    class ElectricEngine : Engine
+    {
+
+    }
+    class GasEngine : Engine
+    {
+
+    }
+    class Battery : PartCar
+    {
+
+    }
+    class Differential : PartCar
+    {
+
+    }
+    class Wheel : PartCar
+    {
+
+    }
+}
