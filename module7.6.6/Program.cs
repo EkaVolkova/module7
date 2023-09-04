c3c19fd11934f22dd417f416a5f3d7a7c2e28fe9
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-04 19:14:57 +0500

    7.6.6 Реализуйте класс-обобщение Record, у которого будут два универсальных параметра: один — для идентификатора записи (Id), другой — для значения записи (Value). Также в классе реализуйте поле Date типа DateTime.

---
+++ module7.6.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Реализуйте класс-обобщение Record, у которого будут два универсальных параметра: 
+/// один — для идентификатора записи (Id), другой — для значения записи (Value). 
+/// Также в классе реализуйте поле Date типа DateTime.
+/// </summary>
+namespace module7._6._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Record<T1, T2>
+    {
+        public T1 Id;
+        public T2 Value;
+        public DateTime Date;
+    }
+}
