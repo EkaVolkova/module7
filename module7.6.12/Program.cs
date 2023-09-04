464193a79f083cf74cfdddf582b8cc9ed767606d
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-04 19:50:13 +0500

    7.6.12 С учётом полученных знаний по наследованию обобщений, дополните схему классов автомобиля, добавив классы для электромобиля и бензинового — ElectricCar и GasCar.

---
+++ module7.6.12/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+/// <summary>
+/// С учётом полученных знаний по наследованию обобщений, дополните схему классов автомобиля, добавив классы для электромобиля и бензинового — ElectricCar и GasCar.
+/// </summary>
+namespace module7._6._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    abstract class Car<TEngine> where TEngine : Engine
+    {
+        public TEngine Engine;
+        public abstract void ChangePart<TPartCar>(TPartCar newPart) where TPartCar : PartCar;
+       
+    }
+    class ElectricCar : Car<ElectricEngine>
+    {
+
+        public override void ChangePart<TPartCar>(TPartCar newPart)
+        {
+
+        }
+    }
+    class GasCar : Car<GasEngine> 
+    {
+
+        public override void ChangePart<TPartCar>(TPartCar newPart)
+        {
+
+        }
+    }
+    abstract class Engine
+    {
+
+    }
+    abstract class PartCar
+    {
+
+    }
+    class ElectricEngine : Engine
+    {
+
+    }
+    class GasEngine : Engine
+    {
+
+    }
+    class Battery : PartCar
+    {
+
+    }
+    class Differential : PartCar
+    {
+
+    }
+    class Wheel : PartCar
+    {
+
+    }
+}
